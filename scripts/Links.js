d3f2e41238c82beac3504ed8e0ddbc551033771d
Ricsaittan <rickytan1019@gmail.com>
2025-04-06 20:56:55 -0600

    Slider added with new events

--- scripts/Links.js
+++ scripts/Links.js
@@ -25,7 +25,3 @@ function blog() {
 function confession() {
     window.open("https://www.pcaac.org/bco/westminster-confession/")
 }
-
-function eventDoc() {
-    window.open("https://docs.google.com/forms/d/e/1FAIpQLSdHsTKCm8k6HsR7rqcvDYvKTvK1y45D5nAI_uRKQnG0O0WWgw/viewform")
-}
